ec639ac7e23c16cae8ad2bdbc9809113b0b7293e
omkareshwar9849 <omkar.varada9849@gmail.com>
2023-07-28 01:44:43 +0530

    final version done

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,11 +5,11 @@ export const AppReducer = (state, action) => {
     let new_expenses = [];
     switch (action.type) {
         case 'ADD_QUANTITY':
-            let updatedqty = false;
+            // let updatedqty = false;
             state.expenses.map((expense)=>{
                 if(expense.name === action.payload.name) {
                     expense.quantity = expense.quantity + action.payload.quantity;
-                    updatedqty = true;
+                    // updatedqty = true;
                 }
                 new_expenses.push(expense);
                 return true;
